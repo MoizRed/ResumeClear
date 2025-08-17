58062b9a2515c7a96ddd0a255399d479187063aa
MoizRed <abdoureduniv2000@gmail.com>
2025-08-17 14:56:08 +0100

    initializing the front end

---
+++ frontend/src/app/components/layout/NavBar.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import Image from "next/image"
+
+
+export default function NavBar(){
+
+    return(
+<div className="flex items-center justify-between p-5 border-b border-cyan-500">
+ {/* TODO : complete the NAVBAR */}
+ 
+  {/* Logo */}
+  <Image
+    src="/ResumeClear.svg"
+    alt="ResumeClear"
+    width={160}
+    height={40}
+    className="cursor-pointer"
+  />
+
+   {/* NAV */}
+
+    
+
+
+
+</div>
+
+
+
+    )
+}
